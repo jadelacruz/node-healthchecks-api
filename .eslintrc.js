d58028507117554e3ebbf4f943d6604f240799ee
Aldrin (Jhon) <jhon.aldrin.delacruz@gmail.com>
2021-02-05 14:41:20 +0800

    change the line space formating for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,67 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "jest/globals": true
+    'env': {
+        'es6': true,
+        'node': true,
+        'jest/globals': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": 2017
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module',
+        'ecmaVersion': 2017
     },
-    "plugins": [
-        "jsdoc",
-        "jest"
+    'plugins': [
+        'jsdoc',
+        'jest'
     ],
-    "globals": {
-        "BDU_NODE_AUTHORIZATION": true,
-        "IS_AUTH": true,
+    'globals': {
+        'BDU_NODE_AUTHORIZATION': true,
+        'IS_AUTH': true,
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'indent': [
+            'error',
             4,
             {
-                "SwitchCase": 1
+                'SwitchCase': 1
             }
         ],
-        "max-len": [
-            "error",
+        'max-len': [
+            'error',
             {
-                "code": 120,
-                "ignoreComments": true
+                'code': 120,
+                'ignoreComments': true
             }
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ],
-        "jsdoc/check-param-names": 1,
-        "jsdoc/check-tag-names": 1,
-        "jsdoc/check-types": 1,
-        "jsdoc/newline-after-description": 1,
-        "jsdoc/require-description-complete-sentence": 1,
-        "jsdoc/require-example": 0,
-        "jsdoc/require-hyphen-before-param-description": 1,
-        "jsdoc/require-param": 1,
-        "jsdoc/require-param-description": 1,
-        "jsdoc/require-param-name": 1,
-        "jsdoc/require-param-type": 1,
-        "jsdoc/require-returns-description": 1,
-        "jsdoc/require-returns-type": 1
+        'jsdoc/check-param-names': 1,
+        'jsdoc/check-tag-names': 1,
+        'jsdoc/check-types': 1,
+        'jsdoc/newline-after-description': 1,
+        'jsdoc/require-description-complete-sentence': 1,
+        'jsdoc/require-example': 0,
+        'jsdoc/require-hyphen-before-param-description': 1,
+        'jsdoc/require-param': 1,
+        'jsdoc/require-param-description': 1,
+        'jsdoc/require-param-name': 1,
+        'jsdoc/require-param-type': 1,
+        'jsdoc/require-returns-description': 1,
+        'jsdoc/require-returns-type': 1
     },
-    "settings": {
-        "jsdoc": {
-            "additionalTagNames": {
-                "customTags": ["async", "extends"]
+    'settings': {
+        'jsdoc': {
+            'additionalTagNames': {
+                'customTags': ['async', 'extends']
             }
         }
     }
--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 
 /**
  * @module node-healthchecks-api
- * @description Implements: https://hootsuite.github.io/health-checks-api/
+ * @description Implements: https://hootsuite.github.io/health-checks-api/.
  */
 module.exports = require('./lib/health-check');
